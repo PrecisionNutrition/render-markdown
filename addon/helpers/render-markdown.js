a9c44f04b4413fed447752088604bf53ba87ba83
Benjamin Borowski <ben@weareokidoki.com>
2020-07-23 10:14:14 -0700

    [ES-129] feat: parse definitions in render-markdown
    Co-authored-by: Graham Anthony <graham@precisionnutrition.com>

--- addon/helpers/render-markdown.js
+++ addon/helpers/render-markdown.js
@@ -4,6 +4,9 @@ import { htmlSafe } from '@ember/string';
 import markdownit from 'markdown-it';
 import attrs from 'markdown-it-attrs';
 
+
+// FIXME: use renderer override
+// SEE: https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md
 function targetLinks(html) {
   let origin = window.location.origin;
 
@@ -22,6 +25,28 @@ function targetLinks(html) {
   return doc.body ? doc.body.innerHTML : '';
 }
 
+// TODO: this could be very slow!
+// change to markdown-it plugin and include interpolations?
+export function parseDefinitions(html) {
+  // SEE: https://regex101.com/r/3Dpban/4
+  const DEFINITIONS_EXPR = /\[definition:[\s+]?(?<definition>.*?)\](?<term>.*?)\[\/definition\]/g
+
+  let match;
+
+  while ((match = DEFINITIONS_EXPR.exec(html))) {
+    let { definition, term } = match.groups;
+    // FIXME: escape content
+    let replacement = `
+      <span class="Definition" data-tippy-title="${term}" data-tippy-content="${definition}">
+        ${term}
+      </span>
+    `;
+    html = html.replace(match[0], replacement);
+  }
+
+  return html;
+}
+
 export function renderMarkdown([raw]) {
   const renderer = markdownit({ html: true }).use(attrs);
 
@@ -30,7 +55,7 @@ export function renderMarkdown([raw]) {
   } else {
     let html = renderer.render(raw);
 
-    html = targetLinks(html);
+    html = targetLinks(parseDefinitions(html));
 
     return htmlSafe(html);
   }
